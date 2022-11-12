1c0d7584d8cb1e615b4e87f18ed718135daa2823
Dmitrij <118051854+DmitrijP1402@users.noreply.github.com>
2022-11-12 19:05:55 +0100

    Update webserver.ino

--- webserver.ino
+++ webserver.ino
@@ -1,30 +1,27 @@
 #include <WiFi.h>
 #include <WebServer.h>
 
-const char* ssid = "ESP 32S";          // Hier den Namen des Netzwerk eingeben (Hier im Code Hotspot von meinem Handy)
-const char* password = "187=):-)";  // Hier Passwort vom Netzwerk eingeben
+const char* ssid = "insert network name";           // Enter the name of the network here
+const char* password = "insert network password";   // Enter password from network here
 
 WebServer server(80);
 
-#define RXp2 16
-#define TXp2 17
+int ScoreOld = 0;  //Score
+int ScoreNew = 0;
 
-int dinoScoreOld = 0;  //Punktzahl
-int dinoScoreNew = 0;
+int idOld = 0;  //ID from player
+int idNew = 1;
 
-int idAlt = 0;  //ID des Spielers
-int idNeu = 1;
+//String nameInput;     //Data Input
+String pointsInput;
 
-//String eingabeName;
-String eingabePunkte; 
-
-int bubbelsortZaehler = 0;  //bubbelsort
-int bubbelsortZaehler1 = 0;
-int zahlBubbelsort = 0;
+int bubbelsortCounterOne = 0;  //bubbelsort
+int bubbelsortCounterTwo = 0;
+int bubbelsortNumber = 0;
 
 //String nameBubbelsort = "";
 
-int dinoScoreArray[1000];  //Arrays die Daten speichern für die Rangliste
+int dinoScoreArray[1000];  //Arrays that store data for the ranking list
 int idArray[1000];
 //String nameArray[1000];
 
@@ -37,22 +34,21 @@ void setup() {
   Serial.begin(115200);
   delay(100);
 
-  Serial.println("Connecting to ");  // Zum oben angegeben Netwerk verbinden
+  Serial.println("Connecting to ");  // Connect to the network
   Serial.println(ssid);
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {  //überürüfen ob eine Verbindung zum Netwerk besteht oder nicht
+  while (WiFi.status() != WL_CONNECTED) { //Check that the NodeMCU ESP32S is connected to the network
     delay(1000);
     Serial.print(".");
   }
 
-  Serial.println("");  //Ausgeben das der NodeMCUESP32S mit dem NEtzwerk verbunden ist
+  Serial.println("");  //Output that the NodeMCU ESP32S is connected to the network
   Serial.println("WiFi connected..!");
 
-  Serial.print("Got IP: ");  //IP Adresse vom NodeMCUESP32S ausgeben
-  Serial.println(WiFi.localIP());
+  Serial.print("Got IP: ");  //Output IP address of NodeMCU ESP32S on serial monitor
 
-  server.on("/", handle_OnConnect);  //Webserver startet und kann über die IP Adresse erreicht werden
+  server.on("/", handle_OnConnect);  //Web server starts and can be reached via the IP address
   server.onNotFound(handle_NotFound);
   server.begin();
   Serial.println("HTTP server started");
@@ -64,62 +60,61 @@ void loop() {
 
 void handle_OnConnect() {
 
-  while (Serial.available() > 0)  //Eingabe des Namen über den Seriellen Monitor
-  {
-    char Zeichen = Serial.read();
-    eingabePunkte += Zeichen;
+  while (Serial.available() > 0) {          //Enter points via the serial monitor
+    char charactersInput = Serial.read();
+    pointsInput += charactersInput;
 
-    if (Zeichen == '\n') {            //Punkte über den Seriellen Monitor eingeben
-      Serial.print(eingabePunkte);        
-      dinoScoreNew = eingabePunkte.toInt();
-      eingabePunkte = "";
-      Serial.print(dinoScoreNew);
+    if (charactersInput == '\n') {
+      ScoreNew = pointsInput.toInt();
+      pointsInput = "";
+      Serial.println(ScoreNew);
     }
   }
 
-  if (dinoScoreNew != dinoScoreOld) {
-    idNeu = idAlt + 1;                  //ID hochzählen
-    idAlt = idNeu;
+  if (ScoreNew != ScoreOld) {
+    idNew = idOld + 1;                  //ID count up
+    idOld = idNew;
 
-    dinoScoreArray[idNeu] = dinoScoreNew;  //Werte in Arrays legen zum speichern
-    idArray[idNeu] = idNeu;
-    //nameArray[idNeu] = eingabeName;
-    //eingabeName = "";
+    dinoScoreArray[idNew] = ScoreNew;  //Put values in arrays to store
+    idArray[idNew] = idNew;
+    //nameArray[idNew] = nameInput;
+    //nameInput = "";
 
-    for (bubbelsortZaehler = 1; bubbelsortZaehler < 1000; bubbelsortZaehler++)  //BubbelSort (Sortieren) von der ID, Punktzahl und dem Namen
+    for (bubbelsortCounterOne = 1; bubbelsortCounterOne < 1000; bubbelsortCounterOne++)  //BubbelSort (Sort) the ID, score and name
     {
-      for (bubbelsortZaehler1 = bubbelsortZaehler; bubbelsortZaehler1 < 1000; bubbelsortZaehler1++) {
-        if (dinoScoreArray[bubbelsortZaehler1] > dinoScoreArray[bubbelsortZaehler]) {
-          zahlBubbelsort = dinoScoreArray[bubbelsortZaehler];
-          dinoScoreArray[bubbelsortZaehler] = dinoScoreArray[bubbelsortZaehler1];
-          dinoScoreArray[bubbelsortZaehler1] = zahlBubbelsort;
+      for (bubbelsortCounterTwo = bubbelsortCounterOne; bubbelsortCounterTwo < 1000; bubbelsortCounterTwo++) {
+        if (dinoScoreArray[bubbelsortCounterTwo] > dinoScoreArray[bubbelsortCounterOne]) {
+          bubbelsortNumber = dinoScoreArray[bubbelsortCounterOne];
+          dinoScoreArray[bubbelsortCounterOne] = dinoScoreArray[bubbelsortCounterTwo];
+          dinoScoreArray[bubbelsortCounterTwo] = bubbelsortNumber;
 
-          zahlBubbelsort = idArray[bubbelsortZaehler];
-          idArray[bubbelsortZaehler] = idArray[bubbelsortZaehler1];
-          idArray[bubbelsortZaehler1] = zahlBubbelsort;
+          bubbelsortNumber = idArray[bubbelsortCounterOne];
+          idArray[bubbelsortCounterOne] = idArray[bubbelsortCounterTwo];
+          idArray[bubbelsortCounterTwo] = bubbelsortNumber;
 
-          /*nameBubbelsort = nameArray[bubbelsortZaehler];
-          nameArray[bubbelsortZaehler] = nameArray[bubbelsortZaehler1];
-          nameArray[bubbelsortZaehler1] = nameBubbelsort;*/
+          /*nameBubbelsort = nameArray[bubbelsortCounterOne];
+            nameArray[bubbelsortCounterOne] = nameArray[bubbelsortCounterTwo];
+            nameArray[bubbelsortCounterTwo] = nameBubbelsort;*/
         }
       }
     }
-    //Daten in den Arrays an den Webserver senden
+    //Send data in the arrays to the web server
     server.send(200, "text/html", SendHTML(dinoScoreArray[1], idArray[1], dinoScoreArray[2], idArray[2], dinoScoreArray[3], idArray[3], dinoScoreArray[4], idArray[4], dinoScoreArray[5], idArray[5], dinoScoreArray[6], idArray[6], dinoScoreArray[7], idArray[7], dinoScoreArray[8], idArray[8], dinoScoreArray[9], idArray[9], dinoScoreArray[10], idArray[10])); //, nameArray[1], nameArray[2], nameArray[3], nameArray[4], nameArray[5], nameArray[6], nameArray[7], nameArray[8], nameArray[9], nameArray[10]
-    dinoScoreOld = dinoScoreNew;
-    dinoScoreNew = 0;
-  } else {  //Daten in den Arrays an den Webserver senden falls sich nichts an der Punktzahl aendert
+    ScoreOld = ScoreNew;
+    ScoreNew = 0;
+  } else {        //Send data in the arrays to the web server if there is no change in the score
     server.send(200, "text/html", SendHTML(dinoScoreArray[1], idArray[1], dinoScoreArray[2], idArray[2], dinoScoreArray[3], idArray[3], dinoScoreArray[4], idArray[4], dinoScoreArray[5], idArray[5], dinoScoreArray[6], idArray[6], dinoScoreArray[7], idArray[7], dinoScoreArray[8], idArray[8], dinoScoreArray[9], idArray[9], dinoScoreArray[10], idArray[10])); // ,nameArray[1], nameArray[2], nameArray[3], nameArray[4], nameArray[5], nameArray[6], nameArray[7], nameArray[8], nameArray[9], nameArray[10]
   }
 }
 
-void handle_NotFound() {  //Server funktioniert nicht/nicht aufrubar
+void handle_NotFound() {                        //Server not working/not available
   server.send(404, "text/plain", "Not found");
 }
 
-// Webseite mit HTML und CSS mit einer Tabbelle & Überschrift &  Daten werden an den vorgesehenen Stellen angezeigt
-String SendHTML(int punktzahl1, float id1, int punktzahl2, float id2, int punktzahl3, float id3, int punktzahl4, float id4, int punktzahl5, float id5, int punktzahl6, float id6, int punktzahl7, float id7, int punktzahl8, float id8, int punktzahl9, float id9, int punktzahl10, float id10) { //, String name1, String name2, String name3, String name4, String name5, String name6, String name7, String name8, String name9, String name10
-  String ptr = "<!DOCTYPE html> <html>\n";
+// Web page with HTML and CSS with a tab & heading & data are displayed in the designated places
+
+  String SendHTML(int scorePosition1, float IdPosition1, int scorePosition2, float IdPosition2, int scorePosition3, float IdPosition3, int scorePosition4, float IdPosition4, int scorePosition5, float IdPosition5, int scorePosition6, float IdPosition6, int scorePosition7, float IdPosition7, int scorePosition8, float IdPosition8, int scorePosition9, float IdPosition9, int scorePosition10, float IdPosition10) { //, String namePosition1, String namePosition2, String namePosition3, String namePosition4, String namePosition5, String namePosition6, String namePosition7, String namePosition8, String namePosition9, String namePosition10
+  String  ptr = "<!DOCTYPE html> <html>\n";
   ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
   ptr += "<title>ESP32 Webserver Dino Co. Ltd.</title>\n";
   ptr += "<style>html {font-family: Calibri; display: inline-block; margin: 0px auto; text-align: center;background-color: black;font-size: 30px;color: white;}\n";
@@ -131,69 +126,69 @@ String SendHTML(int punktzahl1, float id1, int punktzahl2, float id2, int punktz
   ptr += "</head>\n";
   ptr += "<body>\n";
   ptr += "<div id=\"webpage\">\n";
-  ptr += "<h1>Dino Co. Ltd.</h1>\n";
-  ptr += "<table align=center border=5 cellpadding=50><tr><th>Platz</th><th>ID</th><th>Punktzahl</th></tr>\n"; //<th>Name</th>
+  ptr += "<h1>headline of the web page</h1>\n";
+  ptr += "<table align=center border=5 cellpadding=50><tr><th>Position</th><th>ID</th><th>Score</th></tr>\n"; //<th>Name</th>
   ptr += "<tr><td>1</td><td>\n";
-  ptr += (int)id1;
+  ptr += (int)IdPosition1;
   //ptr += "</td><td>\n";
-  //ptr += (String)name1;
+  //ptr += (String)namePosition1;
   ptr += "</td><td>\n";
-  ptr += (int)punktzahl1;
+  ptr += (int)scorePosition1;
   ptr += "</td></tr>\n";
   ptr += "<tr><td>2</td><td>\n";
-  ptr += (int)id2;
+  ptr += (int)IdPosition2;
   //ptr += "</td><td>\n";
-  //ptr += (String)name2;
+  //ptr += (String)namePosition2;
   ptr += "</td><td>\n";
-  ptr += (int)punktzahl2;
+  ptr += (int)scorePosition2;
   ptr += "<tr><td>3</td><td>\n";
-  ptr += (int)id3;
+  ptr += (int)IdPosition3;
   //ptr += "</td><td>\n";
-  //ptr += (String)name3;
+  //ptr += (String)namePosition3;
   ptr += "</td><td>\n";
-  ptr += (int)punktzahl3;
+  ptr += (int)scorePosition3;
   ptr += "<tr><td>4</td><td>\n";
-  ptr += (int)id4;
+  ptr += (int)IdPosition4;
   //ptr += "</td><td>\n";
-  //ptr += (String)name4;
+  //ptr += (String)namePosition4;
   ptr += "</td><td>\n";
-  ptr += (int)punktzahl4;
+  ptr += (int)scorePosition4;
   ptr += "<tr><td>5</td><td>\n";
-  ptr += (int)id5;
+  ptr += (int)IdPosition5;
   //ptr += "</td><td>\n";
-  //ptr += (String)name5;
+  //ptr += (String)namePosition5;
   ptr += "</td><td>\n";
-  ptr += (int)punktzahl5;
+  ptr += (int)scorePosition5;
   ptr += "<tr><td>6</td><td>\n";
-  ptr += (int)id6;
+  ptr += (int)IdPosition6;
   //ptr += "</td><td>\n";
-  //ptr += (String)name6;
+  //ptr += (String)namePosition6;
   ptr += "</td><td>\n";
-  ptr += (int)punktzahl6;
+  ptr += (int)scorePosition6;
   ptr += "<tr><td>7</td><td>\n";
-  ptr += (int)id7;
+  ptr += (int)IdPosition7;
   //ptr += "</td><td>\n";
-  //ptr += (String)name7;
+  //ptr += (String)namePosition7;
   ptr += "</td><td>\n";
-  ptr += (int)punktzahl7;
+  ptr += (int)scorePosition7;
   ptr += "<tr><td>8</td><td>\n";
-  ptr += (int)id8;
+  ptr += (int)IdPosition8;
   //ptr += "</td><td>\n";
-  //ptr += (String)name8;
+  //ptr += (String)namePosition8;
   ptr += "</td><td>\n";
-  ptr += (int)punktzahl8;
+  ptr += (int)scorePosition8;
   ptr += "<tr><td>9</td><td>\n";
-  ptr += (int)id9;
+  ptr += (int)IdPosition9;
   //ptr += "</td><td>\n";
-  //ptr += (String)name9;
+  //ptr += (String)namePosition9;
   ptr += "</td><td>\n";
-  ptr += (int)punktzahl9;
+  ptr += (int)scorePosition9;
   ptr += "<tr><td>10</td><td>\n";
-  ptr += (int)id10;
+  ptr += (int)IdPosition10;
   //ptr += "</td><td>\n";
-  //ptr += (String)name10;
+  //ptr += (String)namePosition10;
   ptr += "</td><td>\n";
-  ptr += (int)punktzahl10;
+  ptr += (int)scorePosition10;
   ptr += "</td></tr></table>\n";
   ptr += "</div>\n";
   ptr += "</body>\n";
